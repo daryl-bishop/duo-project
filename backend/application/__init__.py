9a5d8f78c8c8b4449422c3941166bae9d0a83e69
charlotte <ccartwright@qa.com>
2021-09-03 10:05:08 +0000

    DP-16 #bug fix

--- backend/application/__init__.py
+++ backend/application/__init__.py
@@ -4,7 +4,7 @@ from os import getenv
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:password@database-1.cewazgqmy3qd.eu-west-1.rds.amazonaws.com:3306/users'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://admin:?db1357db?@database-1.cewazgqmy3qd.eu-west-1.rds.amazonaws.com:3306/users'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = str(getenv('SECRET_KEY'))
 db = SQLAlchemy(app)
